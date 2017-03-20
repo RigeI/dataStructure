2026af5a5a9d6e23c2cd81abefd584ecf424fb2c
Rigel <nierunjie@foxmail.com>
2017-03-20 21:51:26 +0800

    修改循环链表

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -18,46 +18,46 @@ struct Node
 template<class DataType>
 class CircularLinkedList
 { 
-// 如果只实现约瑟夫环,可以简化类
-// 只需用数组创建的对象
-// 析构时用约瑟夫环的方法析构,最后删除表头
+    // 如果只实现约瑟夫环,可以简化类
+    // 只需用数组创建的对象
+    // 析构时用约瑟夫环的方法析构,最后删除表头
 
-private:
-	Node<DataType>* first;
-public:
-	CircularLinkedList();
-	CircularLinkedList(DataType arr[],int n);
-	~CircularLinkedList();
-    int Length();
-    void Kill(int num);
-	void PrintList();
+    private:
+        Node<DataType>* first;
+    public:
+        CircularLinkedList();
+        CircularLinkedList(DataType arr[],int n);
+        ~CircularLinkedList();
+        int Length();
+        void Kill(int num);
+        void PrintList();
 };
 
-template<class DataType>
+    template<class DataType>
 CircularLinkedList<DataType>::CircularLinkedList()
 {
-	first=new Node<DataType>;
-	first->next=first;
+    first=new Node<DataType>;
+    first->next=first;
 }
 
-template<class DataType>
+    template<class DataType>
 CircularLinkedList<DataType>::CircularLinkedList(DataType arr[],int n)
 {
-	first=new Node<DataType>;
-	first->next=first;
-	Node<DataType>* r = first;
-	for(int t=0;t<n;t++)
-	{
-		Node<DataType> *s=new Node<DataType>;
+    first=new Node<DataType>;
+    first->next=first;
+    Node<DataType>* r = first;
+    for(int t=0;t<n;t++)
+    {
+        Node<DataType> *s=new Node<DataType>;
         s->data = arr[t];
         s->next=r->next;
         r->next=s;
         r=s;
-	}
+    }
 }
 
 
-template<class DataType>
+    template<class DataType>
 CircularLinkedList<DataType>::~CircularLinkedList()
 {
     Node<DataType> *p=first->next;
@@ -72,7 +72,7 @@ CircularLinkedList<DataType>::~CircularLinkedList()
 }
 
 
-template<class DataType>
+    template<class DataType>
 void CircularLinkedList<DataType>::PrintList()
 {
     Node<DataType>* p = first->next;
@@ -84,7 +84,7 @@ void CircularLinkedList<DataType>::PrintList()
 }
 
 
-template<class DataType>
+    template<class DataType>
 int CircularLinkedList<DataType>::Length()
 {
     Node<DataType>* p=first;
@@ -98,7 +98,7 @@ int CircularLinkedList<DataType>::Length()
 /**
  * 约瑟夫环
  */
-template<class DataType>
+    template<class DataType>
 void CircularLinkedList<DataType>::Kill(int num)
 {
     Node<DataType> *p=first;//工作指针,p->next将被删除
@@ -107,21 +107,21 @@ void CircularLinkedList<DataType>::Kill(int num)
     while(p!=p->next)
     {   
         p=p->next;
-        
+
         if(p==first)
         {
             p=p->next;
         }
-        
+
         t++;
-    
+
         if(t%num==0)
         { 
             if(p->next==first)
             {
                 p=p->next;
             }
-        
+
             q=p->next->next;
             cout<<p->next->data<<endl;
             delete p->next;
@@ -138,17 +138,17 @@ int main()
     if(true)
     {
         //构造数组
-    	int n=40;
-   		int arr[n];
-   		for(int i=0;i<n;i++)
-   		{
-    		arr[i]=i+1;
-    	}
-        
-        //根据数组创建循环链表
-   		CircularLinkedList<int> cll(arr,n);
-        //约瑟夫环,每7个杀死一个人
-   		cll.Kill(7);
+        int n=40;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=i+1;
+        }
 
-       	}
+        //根据数组创建循环链表
+        CircularLinkedList<int> cll(arr,n);
+        //约瑟夫环,每7个杀死一个人
+        cll.Kill(7);
+
+    }
 }  
