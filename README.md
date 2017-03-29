141b74bf5c0d03272027d5aa017e9802aaa71055
Rigel <nierunjie@foxmail.com>
2017-03-29 12:55:38 +0800

    添加丑数

--- README.md
+++ README.md
@@ -26,5 +26,5 @@
 * [汉诺塔](exercise/hanoi.cpp)
 * [括号匹配](exercise/parentheses.cpp)
 * [KMP算法](exercise/kmp.cpp)
-
+* [丑数](exercise/isUgly.cpp)
 
---
+++ exercise/isUgly.cpp
@@ -0,0 +1,31 @@
+// 写一个程序来检测一个整数是不是丑数。
+//
+// 丑数的定义是，只包含质因子 2, 3, 5 的正整数。比如 6, 8 就是丑数，但是 14 不是丑数以为他包含了质因子 7。
+//
+// 1是丑数
+
+#include<iostream>
+using namespace std;
+
+bool isUgly(int num) 
+{
+    if(num==0) return false;
+    else if(num==1) return true;
+    else if(num==2) return true;
+    else if(num==3) return true;
+    else if(num==5) return true;
+
+    if (num%2==0) return isUgly(num/2);
+    else if(num%3==0) return isUgly(num/3);
+    else if(num%5==0) return isUgly(num/5);
+    return false;
+}
+
+
+int main()
+{
+    for(int i=0;i<20;i++)
+    {
+        cout<<i<<(isUgly(i)?"是":"不是")<<"丑数"<<endl;
+    }
+}
