87721e6852a0a69d1a6fb50b8eb155544e8f587c
Rigel <nierunjie@foxmail.com>
2017-08-29 23:44:51 +0800

    更新

---
+++ src/Perm.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+void Swap(char &a,char &b){
+    char temp;
+    temp = a ; a = b ; b = temp;
+}
+
+// [k,m)为进行全排列的区间 
+void Perm(char list[], int k,int m){
+    if(k+1==m){
+        cout<<list<<endl;
+    }
+
+    for(int i=k;i<m;i++){
+        Swap(list[k],list[i]);
+        Perm(list,k+1,m);
+        Swap(list[k],list[i]);
+    }
+}
+
+int main(){
+    char list[] = "abc";
+    Perm(list,0,3);
+}
